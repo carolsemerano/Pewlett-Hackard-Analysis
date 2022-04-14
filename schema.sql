621afb88e39b02b33f73fe11f0a122c702093c12
Carolina Semerano <carol_semerano@live.com>
2022-04-14 17:47:06 -0400

    updating schema

--- schema.sql
+++ schema.sql
@@ -34,3 +34,22 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+CREATE TABLE dept_emp (
+    emp_no int   NOT NULL,
+    dept_no varchar   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,
+    FOREIGN KEY  (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE titles (
+    emp_no int   NOT NULL,
+    title varchar   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,
+    FOREIGN KEY  (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no,title,from_date)
+);
